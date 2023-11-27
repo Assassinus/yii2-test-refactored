b8b852fb644af6dc35886ca743ceef5b276ebb49
aydin <zamanov.aydin@gmail.com>
2023-11-27 12:17:20 +0300

    added di configuration

--- config/web.php
+++ config/web.php
@@ -1,64 +1,84 @@
 <?php
 
+use app\components\export\ExportStrategyFactory;
+use app\components\export\IExportStrategyFactory;
+use app\components\history\EventViewFactory;
+use app\components\history\HistoryListRenderer;
+use app\components\history\IEventViewFactory;
+use app\models\search\HistorySearch;
+use app\models\search\IHistorySearch;
+use app\services\history\detail\DetailService;
+use app\services\history\detail\IDetailService;
+use app\services\history\event\EventService;
+use app\services\history\event\IEventService;
+
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
-    'id' => 'basic',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+    'id'         => 'basic',
+    'basePath'   => dirname(__DIR__),
+    'bootstrap'  => ['log'],
+    'aliases'    => [
+        '@bower'       => '@vendor/bower-asset',
+        '@npm'         => '@vendor/npm-asset',
+        '@historyList' => '@app/widgets/HistoryList/views'
     ],
-    'name' => 'Americor Test',
+    'name'       => 'Americor Test',
+    'container'  => [
+        'definitions' => [
+            IDetailService::class         => DetailService::class,
+            IEventService::class          => EventService::class,
+            IEventViewFactory::class      => EventViewFactory::class,
+            IExportStrategyFactory::class => ExportStrategyFactory::class,
+            IHistorySearch::class         => HistorySearch::class,
+            HistoryListRenderer::class    => HistoryListRenderer::class,
+        ],
+    ],
     'components' => [
-        'request' => [
+        'request'      => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '_KrWQvmDJum_stF3vIO3MgXIyKn-rX28',
         ],
-//        'cache' => [
-//            'class' => 'yii\caching\FileCache',
-//        ],
-        'user' => [
-            'identityClass' => 'app\models\User',
+        //        'cache' => [
+        //            'class' => 'yii\caching\FileCache',
+        //        ],
+        'user'         => [
+            'identityClass'   => 'app\models\User',
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
+        'mailer'       => [
+            'class'            => 'yii\swiftmailer\Mailer',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
-        'log' => [
+        'log'          => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
+            'targets'    => [
                 [
-                    'class' => 'yii\log\FileTarget',
+                    'class'  => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
                 ],
             ],
         ],
-        'db' => $db,
-        /*
-        'urlManager' => [
+        'db'           => $db,
+        'urlManager'   => [
+            'class'           => 'yii\web\UrlManager',
+            'showScriptName'  => false,
             'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
+            'rules'           => array(
+                '<controller:\w+>/<id:\d+>'              => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>'          => '<controller>/<action>',
+            ),
         ],
-        */
     ],
-    'modules' => [
-        'gridview' => [
-            'class' => '\kartik\grid\Module'
-        ]
-    ],
-    'params' => $params,
+    'params'     => $params,
 ];
 
 if (YII_ENV_DEV) {
