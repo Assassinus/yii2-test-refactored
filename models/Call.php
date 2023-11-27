0779c45d8a6e529a4094700e3a6d4d9cc80c93a2
aydin <zamanov.aydin@gmail.com>
2023-11-27 12:11:51 +0300

    refactored models

--- models/Call.php
+++ models/Call.php
@@ -70,16 +70,16 @@ class Call extends ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id' => Yii::t('app', 'ID'),
-            'ins_ts' => Yii::t('app', 'Date'),
-            'direction' => Yii::t('app', 'Direction'),
+            'id'            => Yii::t('app', 'ID'),
+            'ins_ts'        => Yii::t('app', 'Date'),
+            'direction'     => Yii::t('app', 'Direction'),
             'directionText' => Yii::t('app', 'Direction'),
-            'user_id' => Yii::t('app', 'User ID'),
-            'customer_id' => Yii::t('app', 'Customer ID'),
-            'status' => Yii::t('app', 'Status'),
-            'statusText' => Yii::t('app', 'Status'),
-            'phone_from' => Yii::t('app', 'Caller Phone'),
-            'phone_to' => Yii::t('app', 'Dialed Phone'),
+            'user_id'       => Yii::t('app', 'User ID'),
+            'customer_id'   => Yii::t('app', 'Customer ID'),
+            'status'        => Yii::t('app', 'Status'),
+            'statusText'    => Yii::t('app', 'Status'),
+            'phone_from'    => Yii::t('app', 'Caller Phone'),
+            'phone_to'      => Yii::t('app', 'Dialed Phone'),
             'user.fullname' => Yii::t('app', 'User'),
             'customer.name' => Yii::t('app', 'Client'),
         ];
@@ -88,7 +88,7 @@ class Call extends ActiveRecord
     /**
      * @return ActiveQuery
      */
-    public function getCustomer()
+    public function getCustomer(): ActiveQuery
     {
         return $this->hasOne(Customer::class, ['id' => 'customer_id']);
     }
@@ -96,7 +96,7 @@ class Call extends ActiveRecord
     /**
      * @return ActiveQuery
      */
-    public function getUser()
+    public function getUser(): ActiveQuery
     {
         return $this->hasOne(User::class, ['id' => 'user_id']);
     }
@@ -104,7 +104,7 @@ class Call extends ActiveRecord
     /**
      * @return string
      */
-    public function getClient_phone()
+    public function getClient_phone(): string
     {
         return $this->direction == self::DIRECTION_INCOMING ? $this->phone_from : $this->phone_to;
     }
@@ -112,7 +112,7 @@ class Call extends ActiveRecord
     /**
      * @return mixed|string
      */
-    public function getTotalStatusText()
+    public function getTotalStatusText(): mixed
     {
         if (
             $this->status == self::STATUS_NO_ANSWERED
@@ -141,7 +141,7 @@ class Call extends ActiveRecord
      * @param bool $hasComment
      * @return string
      */
-    public function getTotalDisposition($hasComment = true)
+    public function getTotalDisposition($hasComment = true): string
     {
         $t = [];
         if ($hasComment && $this->comment) {
@@ -153,7 +153,7 @@ class Call extends ActiveRecord
     /**
      * @return array
      */
-    public static function getFullDirectionTexts()
+    public static function getFullDirectionTexts(): array
     {
         return [
             self::DIRECTION_INCOMING => Yii::t('app', 'Incoming Call'),
@@ -164,7 +164,7 @@ class Call extends ActiveRecord
     /**
      * @return mixed|string
      */
-    public function getFullDirectionText()
+    public function getFullDirectionText(): mixed
     {
         return self::getFullDirectionTexts()[$this->direction] ?? $this->direction;
     }
@@ -172,11 +172,16 @@ class Call extends ActiveRecord
     /**
      * @return string
      */
-    public function getDurationText()
+    public function getDurationText(): string
     {
         if (!is_null($this->duration)) {
             return $this->duration >= 3600 ? gmdate("H:i:s", $this->duration) : gmdate("i:s", $this->duration);
         }
         return '00:00';
+    }
+
+    public function isAnswered(): bool
+    {
+        return $this->status == self::STATUS_ANSWERED;
     }
 }
