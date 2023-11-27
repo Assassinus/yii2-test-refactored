1acba72cce0b9ef97773935424a75f3e569e5bcc
aydin <zamanov.aydin@gmail.com>
2023-11-27 11:54:46 +0300

    fixed codeStyle && indents used factory for render history list deleted unnecessary _item view

--- widgets/HistoryList/views/main.php
+++ widgets/HistoryList/views/main.php
@@ -1,6 +1,7 @@
 <?php
 
-use app\models\search\HistorySearch;
+use app\components\history\EventViewFactory;
+use app\services\history\event\IEventService;
 use yii\data\ActiveDataProvider;
 use yii\helpers\Html;
 use yii\widgets\ListView;
@@ -8,43 +9,50 @@ use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
 /* @var $dataProvider ActiveDataProvider */
-/* @var $model HistorySearch */
 /* @var $linkExport string */
-
+/* @var $eventService IEventService */
 ?>
 
 <?php Pjax::begin(['id' => 'grid-pjax', 'formSelector' => false]); ?>
 
-<div class="panel panel-primary panel-small m-b-0">
-    <div class="panel-body panel-body-selected">
+    <div class="panel panel-primary panel-small m-b-0">
+        <div class="panel-body panel-body-selected">
 
-        <div class="pull-sm-right">
-            <?php if (!empty($linkExport)) {
-                echo Html::a(Yii::t('app', 'CSV'), $linkExport,
-                    [
-                        'class' => 'btn btn-success',
-                        'data-pjax' => 0
-                    ]
-                );
-            } ?>
+            <div class="pull-sm-right">
+                <?php if (!empty($linkExport)) {
+                    echo Html::a(Yii::t('app', 'CSV'), $linkExport,
+                        [
+                            'class'     => 'btn btn-success',
+                            'data-pjax' => 0
+                        ]
+                    );
+                } ?>
+            </div>
+
         </div>
-
     </div>
-</div>
 
 <?php echo ListView::widget([
-    'dataProvider' => $dataProvider,
-    'itemView' => '_item',
-    'options' => [
-        'tag' => 'ul',
+    'dataProvider'     => $dataProvider,
+    'itemView'         => function ($history) use ($eventService) {
+        $eventViewFactory = new EventViewFactory($history);
+        $itemView = $eventViewFactory->createView($history->event);
+
+        return $itemView->render([
+            'user' => $history->user,
+            'body' => $eventService->getBodyByEventType($history)
+        ]);
+    },
+    'options'          => [
+        'tag'   => 'ul',
         'class' => 'list-group'
     ],
-    'itemOptions' => [
-        'tag' => 'li',
+    'itemOptions'      => [
+        'tag'   => 'li',
         'class' => 'list-group-item'
     ],
     'emptyTextOptions' => ['class' => 'empty p-20'],
-    'layout' => '{items}{pager}',
+    'layout'           => '{items}{pager}',
 ]); ?>
 
 <?php Pjax::end(); ?>
